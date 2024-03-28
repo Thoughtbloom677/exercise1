c09bf044f14165069f2ed01a55ef595d5156a155
Thoughtbloom677 <105999633+Thoughtbloom677@users.noreply.github.com>
2024-03-27 20:51:39 -0500

    Added firstscript.sh

---
+++ firstscript.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Echo the statement "start of my first script"
+echo "start of my first script"
+
+# Create a directory called "devops"
+mkdir devops
+
+# Change directory into the new folder
+cd devops
+
+# List the content of the current directory
+ls
+
+# Create a file called "script.sh" in the new folder
+touch script.sh
+
+# Echo the statement "end of my first script"
+echo "end of my first script"
+
